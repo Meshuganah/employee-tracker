da8007080d697549fdc118a0c6919eea25c397f9
Josh Klein <kleinjjosh74@gmail.com>
2022-05-14 11:57:54 -0400

    added sql queries into util/index and working functions into root index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const { addDepartment } = require('./utils/index');
 require('console.table');
 const Db = require ('./utils/index');
 //const db = require ('./db/connection');
@@ -107,31 +108,163 @@ const mainPrompt = () => {
     ]).then(data => {
         switch(data.choice) {
             case 'viewDepartments':
-                // Db.viewDepartments().then(([results]) => {
-                //     console.table(results)});
-                // mainPrompt();
-                getDepartments();
+                return printDepartments();
+                
             case 'viewRoles':
-                return console.log('Currently viewing all roles');
+                //return console.log('Currently viewing all roles');
+                return printRoles();
+               
             case 'viewEmployees':
-                return console.log('Currently viewing all employees');
+                //return console.log('Currently viewing all employees');
+                return printEmployees();
             case 'addDepartment':
-                return console.log('Currently adding a department');
+                return createDepartment();
+                //return console.log('Currently adding a department');
             case 'addRole':
-                return console.log('Currently adding a role');
+                //return console.log('Currently adding a role');
+                return createRole();
             case 'addEmployee':
-                return console.log('Currently adding an employee');
+                //return console.log('Currently adding an employee');
+                return createEmployee();
             case 'updateEmployee':
-                return console.log('You are now updating employee information');
+                //return console.log('You are now updating employee information');
+                return changeEmployee();
         };
     })
 };
 
-function getDepartments() {
+function printDepartments() {
     Db.viewDepartments()
     .then(([results]) => {
+        console.log('\n')
         console.table(results);
     });
     mainPrompt();
+};
+
+function printRoles() {
+    Db.viewRoles()
+    .then(([results]) => {
+        console.log('\n');
+        console.table(results);
+    });
+    mainPrompt();
+};
+
+function printEmployees() {
+    Db.viewEmployees()
+    .then(([results]) => {
+        console.log('\n')
+        console.table(results);
+    });
+    mainPrompt();
+};
+
+async function createDepartment() {
+    const newDepartment = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: 'What department would you like to add?'
+        }
+    ]);
+    const result = newDepartment.department;
+    Db.addDepartment(result);
+    console.log(`${result} has been added!`)
+    mainPrompt();
+};
+
+async function createRole() {
+    const newRole = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the new role you would like to add?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What will the base salary of this role be?'
+        },
+        {
+            type: 'input',
+            name: 'department_id',
+            message: 'Which department will this role be a part of? Please use department id.'
+        }
+    ]);
+    const newName = newRole.name;
+    const newSalary = newRole.salary;
+    const newDepartmentId = newRole.department_id;
+
+    Db.addRole(newName, newSalary, newDepartmentId);
+    console.log(`${newRole.name} has been added to the database!`);
+    mainPrompt();
+};
+
+async function createEmployee() {
+    const newEmployee = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: `What is this employee's first name?`,
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: `What is this employee's last name?`
+        },
+        {
+            type: 'input',
+            name: 'roleId',
+            message: 'What role will this employee be in? Please use a role Id'
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'Who does this employee report to? Please use the employee Id of the manager, or enter NULL if none.'
+        }
+    ]);
+    const newFirstName = newEmployee.firstName;
+    const newLastName = newEmployee.lastName;
+    const newRoleId = newEmployee.roleId;
+    const newManagerId = newEmployee.newManagerId;
+
+    Db.addEmployee(newFirstName, newLastName, newRoleId, newManagerId);
+    console.log(`${newFirstName} ${newLastName} has been added to the database!`);
+    mainPrompt();
+};
+
+async function changeEmployee() {
+    const updatedEmployee = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: `Which employee's information would you like to update? Please use employee id number.`
+        },
+        {
+            type: 'input',
+            name: 'role',
+            message: 'Which role will this employee be moving into? Please use role id, if role has not changed, use previous id.'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: `What will the employee's salary be? If no change, use old salary.`
+        },
+        // {
+        //     type: 'input',
+        //     name: 'manager',
+        //     message: `Who wil this employee's manager be? Please use employee id of manager, if none set NULL.`
+        // }
+    ]);
+    const employeeId = updatedEmployee.employeeId;
+    const employeeRole = updatedEmployee.role;
+    const employeeSalary = updatedEmployee.salary;
+    //const managerId = updatedEmployee.manager;
+
+    Db.updateEmployee(employeeId, employeeRole, employeeSalary);
+    console.log('Employee information has been updated!');
+    mainPrompt();
 }
+
 mainPrompt();
--- utils/index.js
+++ utils/index.js
@@ -10,6 +10,60 @@ class DbQuery {
             'SELECT * FROM department'
         );
     };
+
+    viewRoles() {
+        return this.db.promise().query(
+            `SELECT role.*, department.name
+            AS department_name
+            FROM role
+            LEFT JOIN department
+            ON role.department_id = department.id`
+        );
+    };
+
+    viewEmployees() {
+        return this.db.promise().query( 
+            `SELECT employees.*, role.title, role.salary, department.name
+            AS role_name
+            FROM employees
+            JOIN role
+            ON employees.role_id = role.id
+            JOIN department
+            ON role.department_id = department.id`        
+        )
+    }
+
+    addDepartment(department) {
+        return this.db.promise().query(
+            `INSERT INTO department (name)
+            VALUES('${department}')`
+        );
+    };
+
+    addRole(name, salary, department_id) {
+        return this.db.promise().query(
+            `INSERT INTO role (name, salary, department_id)
+            VALUES('${name}', '${salary}', '${department_id}')`
+        );
+    };
+
+    addEmployee(firstName, lastName, roleId, managerId) {
+        return this.db.promise().query(
+            `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES('${firstName}', '${lastName}', '${roleId}', '${managerId}')`
+        );
+    };
+
+    updateEmployee(id, role, salary) {
+        return this.db.promise().query(
+            `UPDATE employee SET 
+            role_id = '${role}',
+            salary = '${salary}'
+            WHERE id = '${id}'`
+        );
+    };
 };
+
+
 
 module.exports = new DbQuery(db);
